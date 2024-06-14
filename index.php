ac6a088bd1f2888f6bcb723080581c6741652227
Aaron <114464832+jqt08@users.noreply.github.com>
2024-06-14 17:12:06 +0800

    index.php

--- index.php
+++ index.php
@@ -40,6 +40,8 @@ if (isset($_GET['task'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://pzwiki.net/w/images/3/3e/Moodle_Icon_Zombie.png" type="image/png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&family=Unna:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
